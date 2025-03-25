e2cbe6897aafc2828baecc01e6d6f39488332e62
srolfe26 <rolfe.nielsen@gmail.com>
2025-03-25 08:28:55 -0600

    setup frontend in vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
-import "./styles.css";
-import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
-import Payroll from "./Payroll";
-import logo from "./assets/logo.png";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <nav>
-        <div className="logo-container">
-          <img src={logo} alt="logo" />
-        </div>
-        <div>
-          <NavLink to="/payroll">Payroll</NavLink>
-        </div>
-      </nav>
-      <main>
-        <Routes>
-          <Route path="/payroll" element={<Payroll />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
--- notes.md
+++ notes.md
@@ -1,10 +1,14 @@
-2025-03-24:
+2025-03-24
  - Added git to help me track changes for myself along with gitignore
  - Separated into frontend and backend folders
  - Frontend npm build has several things out of date, look into this
     - Ran `npm audit fix` and things got worse. May re-roll frontend
- - Will build backend in node/express
- - Will add docker compose file to make it all run
- - AI Prompt: In the @backend folder, create a typescript on Node project. Setup a basic Nest.js server with a basic health check endpoint for now
-    - GET http://localhost:3000/health
+ - Built backend in node/express
+    - AI Prompt: In the @backend folder, create a typescript on Node project. Setup a basic Nest.js server with a basic health check endpoint for now
+        - GET http://localhost:3000/health
  - Added a common folder to put types common to both packages
+ - Will add docker compose file to make it all run
+
+2025-03-25
+ - Yep. Let's re-roll the frontend with Vite. Why? There's not a lot there and it'll be a better foundation
+
